9fe81f0f8e0b5e2795a570c9bc5035978387f78f
elly <eleanorz@gmail.com>
2013-10-21 18:21:53 -0700

    print_all works in ex 3

--- oop_basic3.php
+++ oop_basic3.php
@@ -1,55 +1,42 @@
-
 <?php 
-
 // Create a class called 'HTML_Helper' that has the following two methods:
 // print_table -> takes an array that has multiple rows of information (imagine you queried the database and got all the list of users where each row has information like ["first_name" => "Michael", "last_name" => "Choi", "nick_name" => "Sensei"]. This functions takes this multi-dimensional arrays and prints out a beautiful html table with all the information in it. Make the keys appear in the first row (e.g. First Name, Last Name, Nick Name) and make the values of each row appear in the subsequent rows (e.g. Michael, Choi, Sensei).
 // print_select -> as the input, this method takes an array AND the name that should be used for the select tag; the method returns a string that is formatted in HTML. For example, say that $sample_array = ("CA", "WA", "UT", "TX", "AZ", "NY") and we call print_select("state", $sample_array) method. This would return a string that looks like: "<select name="state"><option value="CA">CA</option><option value="WA">WA</option>...</select>"
 // IMPORTANT - usually when you create a class, you don't normally have the methods in the class generate html output. For this assignment though, you will make an exception and have the methods in the class generate html output directly. Classes that are created to help the rendering of html are often called 'Helpers' classes.
 
- class HTML_Helper
- {
- 	var $arr = [];
- 	
- 	function __construct($user_array)
- 	{
- 		$this->arr = $user_array;
- 		echo "<h1>public array below</h1>";
- 	}
+	 class HTML_Helper
+	 {
+	 	var $arr = [];
+	 	
+	 	function __construct(){}
 
- 	function print_table(){
+	 	function print_table($array)
+	 	{
+	 		echo "<table style='border:1px solid green;'>";
+	 		foreach ($array[0] as $key => $value){
+	 			echo "<th>".$key." </th>";
+	 		}
 
- 		var_dump($this->arr);
+	 		foreach ($array as $key => $value) {
+	 			echo "<tr>";
+	 				foreach ($value as $subkey => $subvalue) {
+	 					echo "<td>".$subvalue."</td>";
+	 				}
+	 			echo "</tr>";
+	 		}
+	 		echo "<table>";
+	 	}
 
- 		//this foreach loop declares all the headers
- 		// echo "<table>";
+	 	function print_select($user_selector, $array){
+	 		echo "I am inside print_select";
+	 	}
+	 } 
 
- 		// foreach($arr as $key=>$row)
- 		// {
- 		// 	echo "<th>".$key." </th>";
- 			
- 		// }
- 		
- 		// echo "</table>";
+	 $sample_array=array(
+	 	array("first_name" => "eleanor", "last_name" => "zimm", "nick_name" => "unicorn"),  array("first_name" => "Michael", "last_name" => "Choi", "nick_name" => "Sensei")
+	 	); 
 
- 	}
-
- 	function print_select($user_selector){
-
-
- 		echo "I am inside print_select";
-
-
- 	}
- } 
-
- $sample_array=array('person one'=>array("first_name" => "eleanor", "last_name" => "zimm", "nick_name" => "unicorn"), 'person two'=>array("first_name" => "Michael", "last_name" => "Choi", "nick_name" => "Sensei"));
-
- 
-
- $test=new HTML_Helper($sample_array);
-
- $test->print_table($sample_array);
- // $test->print_select("juices");
-
-
+	 $test=new HTML_Helper();
+	 $test->print_table($sample_array);
+	 $test->print_select("juices",$sample_array);
  ?>
